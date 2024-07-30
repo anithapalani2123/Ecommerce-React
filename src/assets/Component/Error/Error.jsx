66293883eded3b0731e8de3f390ee6dd70015f0c
anithapalani2123 <anithapalani2004april@gmail.com>
2024-07-30 20:51:07 +0530

    error page

---
+++ src/assets/Component/Error/Error.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Error.css'
+
+const Error = () => {
+  return (
+    <div className='errorContainer'>
+        <div className="error">
+            <div className='errorBox'>
+                <p id='errorStatus'>404</p>
+                <p id='errorMessage'>page not found </p>
+
+            </div>
+            
+
+        </div>
+        
+        
+      
+    </div>
+  )
+}
+
+export default Error
--- src/assets/Component/Routes/Routes.jsx
+++ src/assets/Component/Routes/Routes.jsx
@@ -7,6 +7,7 @@ import ProductDetail from '../ProductDetail/ProductDetail';
 import Add from '../Add/Add';
 import Update from '../Update/Update';
 import Otp from '../Otp/Otp';
+import Error from '../Error/Error';
 const RoutesPath = () => {
   return (
     <div>
@@ -18,6 +19,7 @@ const RoutesPath = () => {
             <Route path='/product/update/:id' element={<Update/>}></Route>
             <Route path='/product/:id' element={<ProductDetail/>}></Route>
             <Route path='/auth/otp/:id' element={<Otp/>}></Route>
+            <Route path='*' element={<Error/>}></Route>
         </Routes>
         
 
