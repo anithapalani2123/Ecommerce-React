af73c529bf284fc1833887b03658fd874dadf109
anithapalani2123 <anithapalani2004april@gmail.com>
2024-07-31 13:17:53 +0530

    Check: check 01

--- src/assets/Component/Routes/Routes.jsx
+++ src/assets/Component/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Routes,Route} from 'react-router-dom';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
@@ -9,18 +9,30 @@ import Update from '../Update/Update';
 import Otp from '../Otp/Otp';
 import Error from '../Error/Error';
 const RoutesPath = () => {
+  const [role,setRole]=useState("")
+  useEffect(()=>{
+    setRole(sessionStorage.getItem('role'));
+  },[])
   return (
     <div>
         <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/auth/signin' element={<Login/>}></Route>
             <Route path='/auth/signup' element={<Register/>}></Route>
-            <Route path='/product/new' element={<Add/>}></Route>
-            <Route path='/product/update/:id' element={<Update/>}></Route>
             <Route path='/product/:id' element={<ProductDetail/>}></Route>
             <Route path='/auth/otp/:id' element={<Otp/>}></Route>
             <Route path='*' element={<Error/>}></Route>
-        </Routes>
+        
+        {
+          role && role==='admin' && 
+          <div>
+            <Route path='/product/new' element={<Add/>}></Route>
+            <Route path='/product/update/:id' element={<Update/>}></Route>
+            </div>
+            
+         
+        }
+         </Routes>
         
 
       
